79d4d64b64601cdba7e1aaf1316e4c88209d1176
Phong Diep <phongdiep0712@gmail.com>
2023-03-12 15:53:12 -0700

    Created sort.py file and finished implementing the sort_dictionary function

---
+++ sort.py
@@ -0,0 +1,10 @@
+def sort_dictionary(dict):
+    listOfItems = list(dict.items())
+    def sortAge(e):
+        return e[1][1]
+    listOfItems.sort(key=sortAge)
+    result = []
+    for i in listOfItems:
+        tup = (i[0], i[1][0])
+        result.append(tup)
+    return result
